ef8a240a9408bb175ca339edf632e6f07b1323b7
destiny2222 <oghenekumeerieme@gmail.com>
2026-01-29 23:58:20 +0100

    fix: Update middleware to validate CSRF tokens for stripe routes and add webhook secret to stripe configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             // 'http://example.com/foo/bar',
             // 'http://example.com/foo/*',
             'api/*',
+            'stripe/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
